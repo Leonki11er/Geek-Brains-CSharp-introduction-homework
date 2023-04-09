3553c75771645fe2478cabd8302536d79ff14f9a
Leonid_Levenko <leonidustal@gmail.com>
2023-04-09 23:08:30 +0700

    Lesson 1 task 4 done

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите положительное целое число N для вывода всех четных чисел от 1 до N: ");
+int number = int.Parse(Console.ReadLine()!);
+for(int i = 2; i<=number;i=i+2)
+{
+    Console.Write($"{i}, ");
+}
