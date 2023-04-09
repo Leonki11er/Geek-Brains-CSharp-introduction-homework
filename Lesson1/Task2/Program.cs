b465588d206cc066e874508c0b05f8cab701a4e1
Leonid_Levenko <leonidustal@gmail.com>
2023-04-09 22:56:03 +0700

    Lesson 1 task 2 done

---
+++ Lesson1/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine()!);
+Console.Write("Введите третье число: ");
+int thirdNumber = int.Parse(Console.ReadLine()!);
+if(firstNumber>secondNumber&&firstNumber>thirdNumber)
+{
+    Console.WriteLine($"max = {firstNumber}");
+}
+else if(secondNumber>firstNumber&&secondNumber>thirdNumber)
+{
+    Console.WriteLine($"max = {secondNumber}");
+}
+else 
+{
+    Console.WriteLine($"max = {thirdNumber}");
+}
