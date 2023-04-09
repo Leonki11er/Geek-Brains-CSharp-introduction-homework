003678e0b4ca852f1a5cf07fe450c7715dbc1a65
Leonid_Levenko <leonidustal@gmail.com>
2023-04-10 00:53:02 +0700

    Lesson 2 task 3 done

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿void WhatsTheDay()
+{
+   Console.Write("Введите порядковый номер дня недели: ");
+    int number = int.Parse(Console.ReadLine()!);
+    if(number>7||number<1)
+    {
+        Console.WriteLine("Необходимо ввести число от 1 до 7");
+        WhatsTheDay();
+    }
+    else if (number<6)
+    {
+        Console.WriteLine("Будний день");
+    }
+    else
+    {
+        Console.WriteLine("Выходной");
+    } 
+}
+WhatsTheDay();
