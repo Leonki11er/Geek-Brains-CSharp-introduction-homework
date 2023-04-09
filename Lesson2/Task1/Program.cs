c5538f432f6624d9516fd72b1d7beee297009d1c
Leonid_Levenko <leonidustal@gmail.com>
2023-04-10 00:23:27 +0700

    Lesson 2 task 1 done

--- Lesson2/Task1/Program.cs
+++ Lesson2/Task1/Program.cs
@@ -1,5 +1,18 @@
-﻿Console.Write("Введите трехзначное число: ");
-int number = int.Parse(Console.ReadLine()!);
-number = number%100;
-number = number/10;
-Console.WriteLine($"вторая цифра числа - {number}");
+﻿void PrintSecondNumber()
+{
+    Console.Write("Введите трехзначное число: ");
+    int number = int.Parse(Console.ReadLine()!);
+    if(number>99 && number<1000)
+    {
+        number = number%100;
+        number = number/10;
+        Console.WriteLine($"вторая цифра числа - {number}");
+    }
+    else
+    {
+        Console.WriteLine("Необходимо ввести трехзначное число");
+        PrintSecondNumber();
+    }
+    
+}
+PrintSecondNumber();
