09739e4ff55d12b73580db624b10777c3499a1af
Leonid_Levenko <leonidustal@gmail.com>
2023-04-10 00:03:11 +0700

    Lesson 2 task 1 done

---
+++ Lesson2/Task1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+number = number%100;
+number = number/10;
+Console.WriteLine($"вторая цифра числа - {number}");
