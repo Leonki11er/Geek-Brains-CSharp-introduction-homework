78fe37a4e7f9a45c95f6ce86e0814d938931fa14
Leonid_Levenko <leonidustal@gmail.com>
2023-04-09 23:01:10 +0700

    Lesson 1 task 3 done

---
+++ Lesson1/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число для проверки на четность: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if(number%2==0)
+{
+    Console.WriteLine($"{number} - четное число");
+}
+else 
+{
+    Console.WriteLine($"{number} - нечетное число");
+}
