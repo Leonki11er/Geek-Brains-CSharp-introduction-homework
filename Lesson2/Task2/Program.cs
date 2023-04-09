dcfa09092e8112c6939484498621c3b014dfdd9c
Leonid_Levenko <leonidustal@gmail.com>
2023-04-10 00:39:12 +0700

    Lesson 2 task 2 done

---
+++ Lesson2/Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите целое число для определения третьей цифры: ");
+int number = int.Parse(Console.ReadLine()!);
+if(number<100&&number>-100)
+{
+    Console.WriteLine("третья цифра не найдена");
+}
+else
+{
+    while(number>1000 || number < -1000)
+    {
+        number = number/10;
+    }
+    number = number%10;
+    Console.WriteLine($"третья цифра числа - {number}");
+}
