7c65b3398be1d3b5fe6d41398127f4f085042c8a
Leonid_Levenko <leonidustal@gmail.com>
2023-04-15 22:08:53 +0700

    Lesson 3 task 1 done

---
+++ Lesson3/Task_1/Program.cs
@@ -0,0 +1,33 @@
+﻿void Main()
+{
+    Console.Write("Введите пятизначное число для проверки является ли оно палиндромом: ");
+    int number = int.Parse(Console.ReadLine()!);
+    if(number<10000||number>99999)
+    {
+       Console.WriteLine("Необходимо ввести пятизначное число");
+       Main(); 
+    }
+    bool IsPalindrome(int number)
+    {
+        bool isPal=false;
+        int num1 = number / 10000 % 10;
+        int num2 = number / 1000 % 10;
+        int num4 = number / 10 % 10;
+        int num5 = number % 10;
+        if  (num1 == num5 && num2 == num4)
+        {
+            isPal=true;
+        }
+        return isPal;
+    }
+    if(IsPalindrome(number))
+    {
+        Console.WriteLine($"Число {number} палиндром");
+    } 
+    else
+    {
+        Console.WriteLine($"Число {number} не является палиндромом");
+    }
+}
+
+Main();
