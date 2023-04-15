e8d7cca69fbe8cc1483e50a69670c6e13903a51c
Leonid_Levenko <leonidustal@gmail.com>
2023-04-15 22:31:44 +0700

    Lesson 3 task 2 done

---
+++ Lesson3/Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите координаты двух точек для определения расстояния между ними: ");
+Console.Write("X1: ");
+double x1 = double.Parse(Console.ReadLine()!);
+Console.Write("Y1: ");
+double y1 = double.Parse(Console.ReadLine()!);
+Console.Write("Z1: ");
+double z1 = double.Parse(Console.ReadLine()!);
+Console.Write("X2: ");
+double x2 = double.Parse(Console.ReadLine()!);
+Console.Write("Y2: ");
+double y2 = double.Parse(Console.ReadLine()!);
+Console.Write("Z2: ");
+double z2 = double.Parse(Console.ReadLine()!);
+double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)+ Math.Pow(z2 - z1, 2));
+Console.WriteLine($"расстояние между точкой 1({x1},{y1},{z1}) и точкой 2({x2},{y2},{z2}) равно {Math.Round(distance,2)}");
