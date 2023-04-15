6f128619665b2b6facd069aff0d0bce013a398bb
Leonid_Levenko <leonidustal@gmail.com>
2023-04-15 22:35:28 +0700

    Lesson 3 task 3 done

---
+++ Lesson3/Task_3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите целое число для вывода таблицы кубов от 1 до введенного числа: ");
+int number = int.Parse(Console.ReadLine()!);
+for(int i =1;i<=number;i++)
+{
+    int cube = i*i*i;
+    Console.WriteLine($"куб числа {i} равен {cube}");
+}
