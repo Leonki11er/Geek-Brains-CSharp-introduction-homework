de137ddbd95f32674a91dffdb9c664f04b4d39c3
Leonid_Levenko <leonidustal@gmail.com>
2023-04-19 10:51:28 +0700

    Lesson 4 task 3 done

---
+++ Lesson4/Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+
+int[] CreateRandomArray(int arrayLength, int minRandom, int maxRandom)
+{
+    Random random = new Random();
+    int[] array = new int[arrayLength];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(minRandom, maxRandom);
+    }
+    return array;
+}
+
+int[] array = CreateRandomArray(8,-99,100);
+PrintArray(array);
