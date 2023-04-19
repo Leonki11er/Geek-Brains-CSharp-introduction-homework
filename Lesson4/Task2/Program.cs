fb9c174d63123d37ddf9a60971ed1eae7ca06113
Leonid_Levenko <leonidustal@gmail.com>
2023-04-19 10:51:03 +0700

    Lesson 4 task 2 done

---
+++ Lesson4/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿int EnterNumber(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int Sum(int number)
+{
+    int sum = 0;
+    if(number<10) return number;
+    while(number/10!=0)
+    {   
+        sum += number%10;
+        number/=10;
+    }
+    sum += number;
+    return sum;
+}
+Console.WriteLine($"Сумма цифр числа равна {Sum(EnterNumber("Введите число для нахождения суммы входящих в него цифр: "))}");
