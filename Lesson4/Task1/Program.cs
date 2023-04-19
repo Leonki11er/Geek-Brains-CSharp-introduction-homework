d4075b41f31886214cb4050f67c4da6a938cb7da
Leonid_Levenko <leonidustal@gmail.com>
2023-04-19 10:50:35 +0700

    Lesson 4 task 1 done

---
+++ Lesson4/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int EnterNumber(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int Exponentiate(int a, int b)
+{
+    int result;
+    result = a*a;
+    for(int i = 2; i<b;i++)    
+        result*=a;
+    return result;
+}
+Console.WriteLine($"Результат: {Exponentiate(EnterNumber("Введите число для возведения в степень: "),EnterNumber("Введите степень: "))}");
