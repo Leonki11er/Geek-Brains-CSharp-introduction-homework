804a3f21460b7ee61083c0f06af968634bce8538
Leonid_Levenko <leonidustal@gmail.com>
2023-04-22 20:41:16 +0700

    Lesson 5 task 1 done

---
+++ Lesson5/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+
+int[] CreateRandomArray(int arrayLength, int minRandom, int maxRandom)
+{
+    Random random = new Random();
+    int[] array = new int[arrayLength];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(minRandom, maxRandom);
+    }
+    return array;
+}
+
+int FindEven(int[] array)
+{
+    int counter = 0;
+    foreach (int item in array)
+        if(item%2==0) counter++;
+    return counter;
+}
+int[]array = CreateRandomArray(15,100,1000);
+PrintArray(array);
+Console.WriteLine($"в данном масcиве {FindEven(array)} четных чисел");
