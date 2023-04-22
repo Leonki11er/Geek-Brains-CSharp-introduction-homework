df5469cb64155f0331a0909344b782b206c0f825
Leonid_Levenko <leonidustal@gmail.com>
2023-04-22 20:42:01 +0700

    Lesson 5 task 3 done

---
+++ Lesson5/Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+
+int[] CreateRandomArray(int arrayLength, int minRandom, int maxRandom)
+{
+    Random random = new Random();
+    int[] array = new int[arrayLength];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(minRandom, maxRandom);
+    }
+    return array;
+}
+
+int FindMaxElement(int[] array)
+{
+    int max = array[0];
+    for(int i =0; i<array.Length;i++)
+        if(array[i]>max)max=array[i];
+    return max;
+}
+int FindMinElement(int[] array)
+{
+    int min = array[0];
+    for(int i =0; i<array.Length;i++)
+        if(array[i]<min)min=array[i];
+    return min;
+}
+
+int[] array = CreateRandomArray(20,1,100);
+PrintArray(array);
+Console.WriteLine($"Максимальный элемент массива - {FindMaxElement(array)}, минимальный элемент массива - {FindMinElement(array)}, разница элементов - {FindMaxElement(array) - FindMinElement(array)}");
