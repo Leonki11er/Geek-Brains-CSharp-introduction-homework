2339766877406562a7e05ed9095df002f1bc308e
Leonid_Levenko <leonidustal@gmail.com>
2023-05-01 19:23:18 +0700

    Lesson 7 task 3 done

---
+++ Lesson7/Task3/Program.cs
@@ -0,0 +1,58 @@
+﻿void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i,j]}|");
+        Console.WriteLine();    
+    } 
+    Console.WriteLine();
+}
+
+void PrintValues(Double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]}|");
+    Console.WriteLine();
+}
+
+int[,] CreateRandomArray(int arrayColumns,int arrayRows, int minRandom, int maxRandom)
+{
+    Random random = new Random();
+    int[,] array = new int[arrayColumns,arrayRows];
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i,j] = random.Next(minRandom, maxRandom);
+    
+    return array;
+}
+
+int EnterNumber(string message)
+{
+    Console.Write(message);
+    int a = int.Parse(Console.ReadLine()!);
+    return a;
+}
+
+Double AveregeColumnValue(int[,]array,int column)
+{
+    int columnSum=0;
+    for (int j = 0; j < array.GetLength(1); j++)
+        columnSum += array[j,column];
+    return Math.Round(Convert.ToDouble(columnSum)/Convert.ToDouble(array.GetLength(1)),1);
+}
+
+Double[] AverageValuesOfArrayColumns(int[,]array)
+{
+    Double[] values = new Double[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+        values[i]=AveregeColumnValue(array,i);
+    return values;
+}
+int arrayColumns = EnterNumber("Введите количество столбцов: ");
+int arrayRows = EnterNumber("Введите количество строк: ");
+int minRandom = EnterNumber("Введите минимум элемента: ");
+int maxRandom = EnterNumber("Введите максимум элемента: ");
+int[,]array=CreateRandomArray(arrayColumns,arrayRows,minRandom,maxRandom);
+PrintArray(array);
+PrintValues(AverageValuesOfArrayColumns(array));
