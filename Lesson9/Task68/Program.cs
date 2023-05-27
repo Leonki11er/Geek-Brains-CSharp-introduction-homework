ec9282710d169ad7d3b9ceced66e151beb5e6bc6
Leonid_Levenko <leonidustal@gmail.com>
2023-05-27 18:28:11 +0700

    Lesson 9 task 68 done

---
+++ Lesson9/Task68/Program.cs
@@ -0,0 +1,17 @@
+﻿int EnterNumber(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int Ackermann(int m, int n)
+{
+    if (m == 0)return n + 1;
+    else if (n == 0 && m > 0)return Ackermann(m - 1, 1);
+    else return (Ackermann(m - 1, Ackermann(m, n - 1)));
+}
+
+int m = EnterNumber("Введите аругумент М для функции Аккермана: ");
+int n = EnterNumber("Введите аругумент N для функции Аккермана: ");
+Console.WriteLine($"Значение функции - {Ackermann(m, n)}");
