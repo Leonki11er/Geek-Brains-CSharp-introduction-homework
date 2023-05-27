da49080ba7dbb37736e8d07440e09ea9fc8cb5cc
Leonid_Levenko <leonidustal@gmail.com>
2023-05-27 17:58:07 +0700

    Lesson 9 task 66 done

---
+++ Lesson9/Task66/Program.cs
@@ -0,0 +1,22 @@
+﻿int EnterNumber(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+void SumBetweenTwoNumbers(int firstNumber, int secondNumber, int counter, int sum = 0)
+{
+    if(counter<=-1)
+    {   
+        Console.WriteLine($"сумма чисел - {sum}");
+        return;
+    }
+    sum += firstNumber++;
+    counter--;
+    SumBetweenTwoNumbers(firstNumber, secondNumber,counter, sum);
+}
+
+int firstNumber = EnterNumber("Введите первое число промежутка для вычисления суммы элементов: ");
+int secondNumber = EnterNumber("Введите второе число промежутка: ");
+SumBetweenTwoNumbers(firstNumber, secondNumber,secondNumber-firstNumber);
