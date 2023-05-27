9c9ed8af5f8f91227b06075a16dd355e6c397c35
Leonid_Levenko <leonidustal@gmail.com>
2023-05-27 16:48:54 +0700

    Lesson 9 task 64 done

---
+++ Lesson9/Task64/Program.cs
@@ -0,0 +1,16 @@
+﻿int EnterNumber(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+void PrintNumbers(int number)
+{
+    if(number<1)return;
+    Console.Write($"{number--} ");
+    PrintNumbers(number);
+}
+
+int number = EnterNumber("Введите число для вывода на экран от этого числа до 1: ");
+PrintNumbers(number);
